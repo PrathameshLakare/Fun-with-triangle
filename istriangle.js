806438dee4018dd28a639460c8734a1439f30968
PrathameshLakare <prathameshlakare001@gmail.com>
2021-10-22 18:57:41 +0530

    creating is triangle function

---
+++ istriangle.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll('.angle-input');
+const isTriangleBtn = document.querySelector('#is-triangle-btn');
+const outputEl = document.querySelector('#output');
+
+function calculateSumOfAngles(angel1, angle2, angle3) {
+    const sumOfAngles = angel1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+function isTriangle() {
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    if (sumOfAngles === 180) {
+        outputEl.innerText = "It is a triangle.";
+    } else {
+        outputEl.innerText = "It is not a triangle.";
+    }
+}
+
+isTriangleBtn.addEventListener("click", isTriangle);
